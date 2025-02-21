42cf659b33a70473ee0bb5ece8d020b4aa663cc0
Anirban Basak <jeetbasak2002@gmail.com>
2025-02-21 10:23:46 +0530

    feat - how to pass data to the directives

--- src/app/directives/host-listener-and-binding.directive.ts
+++ src/app/directives/host-listener-and-binding.directive.ts
@@ -1,19 +1,35 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHostListenerAndBinding]',
 })
-export class HostListenerAndBindingDirective {
+export class HostListenerAndBindingDirective implements OnInit {
+  // Default parameters which can be passed from the parent
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'lightcoral';
+
   // Binding the variable to the style.backgroudColor property of the host element
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string =
+    this.defaultColor;
+
+  // Initializing the default color after the view is initialized
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   // Styling the element using the host binding object when the host has a hover event
   @HostListener('mouseenter') onMouseEnter(event: Event) {
-    this.backgroundColor = 'lightcoral';
+    this.backgroundColor = this.highlightColor;
   }
 
   // Styling the element using the host binding object when the host has a mouse leave or hover leave event
   @HostListener('mouseleave') onMouseLeave(event: Event) {
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
