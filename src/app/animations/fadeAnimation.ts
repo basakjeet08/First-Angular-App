6e42189c4872dad384f90bc4683f211e18ffd412
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-14 16:22:48 +0530

    feat : Added left slide in animation and right slide out animation

--- src/app/animations/fadeAnimation.ts
+++ src/app/animations/fadeAnimation.ts
@@ -8,3 +8,12 @@ export const fadeAnimation = trigger('fadeAnimation', [
   ]),
   transition(':leave', [animate('1s ease-out', style({ opacity: 0 }))]),
 ]);
+
+// This is the global animation for slide in from left and slide out to right
+export const leftSlideAnimation = trigger('leftSlideAnimation', [
+  transition(':enter', [
+    style({ transform: 'translateX(-200px)' }),
+    animate('300ms ease-in', style({ transform: 'translateX(0)' })),
+  ]),
+  transition(':leave', [animate('300ms ease-out', style({ transform: 'translateX(200px)' }))]),
+]);
--- src/app/components/animation-testing/animation-testing.component.ts
+++ src/app/components/animation-testing/animation-testing.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-import { fadeAnimation } from 'src/app/animations/fadeAnimation';
+import {
+  fadeAnimation,
+  leftSlideAnimation,
+} from 'src/app/animations/fadeAnimation';
 
 @Component({
   selector: 'app-animation-testing',
   templateUrl: './animation-testing.component.html',
   styleUrls: ['./animation-testing.component.css'],
-  animations: [fadeAnimation],
+  animations: [fadeAnimation, leftSlideAnimation],
 })
 export class AnimationTestingComponent {
   // This variable dictates the component visibility
   isFadeVisible: boolean = false;
+  isLeftSlideVisible : boolean = false;
 
   // This function is invoked when the button is clicked
   toggleFade() {
     this.isFadeVisible = !this.isFadeVisible;
+  }
+
+  // This function is invoked when the left slide animation button is clicked
+  toggleLeftSlide(){
+    this.isLeftSlideVisible = !this.isLeftSlideVisible;
   }
 }
